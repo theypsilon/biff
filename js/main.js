e8f58ad1f052b696bd4c5b0c097d7e1b16c2a87a
José manuel Barroso Galindo <theypsilon@gmail.com>
2018-04-29 07:46:30 +0200

    background color

--- js/main.js
+++ js/main.js
@@ -10,20 +10,19 @@
 	});
 	var movies = $('.movies');
 	var trailers = $('.trailers');
-	var chat = $('.chat');
+	var button_movies = $('.button-movies');
+	var button_trailers = $('.button-trailers');
+	var selected_button = undefined;
 	window.show_movies = function() {
 		movies.show(1000);
 		trailers.hide(1000);
-		chat.hide(1000);
+		button_movies.addClass('button-pushed');
+		button_trailers.removeClass('button-pushed');
 	};
 	window.show_trailers = function() {
 		movies.hide(1000);
 		trailers.show(1000);
-		chat.hide(1000);
-	};
-	window.show_chat = function() {
-		movies.hide(1000);
-		trailers.hide(1000);
-		chat.show(1000);
+		button_movies.removeClass('button-pushed');
+		button_trailers.addClass('button-pushed');
 	};
 });
