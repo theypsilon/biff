a6ef4ba9c6f5c24d9457fa99b8afe25762dfd67c
José manuel Barroso Galindo <theypsilon@gmail.com>
2018-04-29 06:36:10 +0200

    Fix

--- js/main.js
+++ js/main.js
@@ -3,27 +3,27 @@
 		var parent = $(this).parent();
 		var content = $(".filmcontent", parent);
 		if (content.is(":visible")) {
-			content.hide();
+			content.hide(600);
 		} else {
-			content.show();
+			content.show(600);
 		}
 	});
 	var movies = $('.movies');
 	var trailers = $('.trailers');
 	var chat = $('.chat');
 	window.show_movies = function() {
-		movies.show();
-		trailers.hide();
-		chat.hide();
+		movies.show(1000);
+		trailers.hide(1000);
+		chat.hide(1000);
 	};
 	window.show_trailers = function() {
-		movies.hide();
-		trailers.show();
-		chat.hide();
+		movies.hide(1000);
+		trailers.show(1000);
+		chat.hide(1000);
 	};
 	window.show_chat = function() {
-		movies.hide();
-		trailers.hide();
-		chat.show();
+		movies.hide(1000);
+		trailers.hide(1000);
+		chat.show(1000);
 	};
 });
