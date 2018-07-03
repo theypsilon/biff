bf3f3e4010fbc9c99f20395bfcef314bcc6dafc4
José Manuel Barroso Galindo <theypsilon@gmail.com>
2018-07-03 18:54:27 +0200

    using scripts

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 ;$(document).ready(function() {
+	var template_video_youtube = $('#template_video_youtube').html();
 	$('.filmhead').click(function() {
 		var parent = $(this).parent();
-		var content = $(".filmcontent", parent);
-		if (content.is(":visible")) {
+		var content = $('.filmcontent', parent);
+		if (content.is(':visible')) {
 			content.hide(600);
 		} else {
 			content.show(600);
+		}
+		var script = $('script', content);
+		if (script.length) {
+			var video_url = script.html();
+			var youtube_iframe = template_video_youtube.replace('/VIDEO_URL_HERE/gi', video_url);
+			content.append(youtube_iframe);
+			script.remove();
 		}
 	});
 	var movies = $('.movies');
